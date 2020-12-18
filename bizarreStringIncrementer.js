71d28907f2fda33ce409f22bb7e97d5ed4c00384
Rasika Hatwar <rasikahatwar13@gmail.com>
2020-12-18 16:24:43 +0530

    string incrementer updated

--- bizarreStringIncrementer.js
+++ bizarreStringIncrementer.js
@@ -1,1 +1,24 @@
 // Start your implementation here
+
+function bizarreStringIncrementer(str) {
+const regex =/\d+$/;
+let matchResult = regex.test(str);
+
+const incrementNumber=numStr=>{
+  const next =String(parseInt(numStr,10)+1);
+  if(next.length < numStr.length){
+    const pad = "0".repeat(numStr.length-next.length);
+    return pad.concat(next);
+  }
+  return next;
+}
+
+if (!matchResult) return str+=1;
+else str = str.replace(regex,incrementNumber);
+
+return str;
+}
+
+export default bizarreStringIncrementer
+
+
